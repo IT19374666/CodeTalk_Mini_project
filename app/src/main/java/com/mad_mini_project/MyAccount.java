344ff8605d125b2ab0020a677f5e14f8942fb6de
IT19395906 <IT19395906@my.sliit.lk>
2020-10-03 10:53:14 +0530

    Transaction

--- app/src/main/java/com/mad_mini_project/MyAccount.java
+++ app/src/main/java/com/mad_mini_project/MyAccount.java
@@ -3,29 +3,13 @@ package com.mad_mini_project;
 
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.content.Intent;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 
 
 public class MyAccount extends AppCompatActivity {
@@ -59,7 +43,7 @@ public class MyAccount extends AppCompatActivity {
                         startActivity(new Intent(getApplicationContext(), MyAccount.class));
                         return true;
                     case R.id.Transaction:
-                        startActivity(new Intent(getApplicationContext(), MyAccount.class));
+                        startActivity(new Intent(getApplicationContext(), My_Transactions.class));
                         return true;
                 }
 
